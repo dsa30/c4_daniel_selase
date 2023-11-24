080e9ac0d27f6eb21e7f74567d6e8f6574ec3d24
Daniel <dselasea@gmail.com>
2023-11-24 17:46:35 +0000

    Add hello world

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+     <h1>Hello World</h1>
+   <p>I am Daniel Selase Anyidoho</p>
+  </div>
+  )
+}
+
+export default App
